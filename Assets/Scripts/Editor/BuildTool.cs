60f0499198e9fc6ff119f2b46bbe6fbfbe493f11
Summer <790390404@qq.com>
2021-01-14 23:13:03 +0800

    Init the repo

---
+++ Assets/Scripts/Editor/BuildTool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildTool : Editor
+{
+    [MenuItem("Tools/BuildBundleForWindows")]
+    static void BundleBuildWindows()
+    {
+        Build(BuildTarget.StandaloneWindows);
+    }
+    [MenuItem("Tools/BuildBundleForAndroid")]
+    static void BundleBuildAndroid()
+    {
+        Build(BuildTarget.Android);
+    }
+    [MenuItem("Tools/BuildBundleForiOS")]
+    static void BundleBuildiOS()
+    {
+        Build(BuildTarget.iOS);
+    }
+    static void Build(BuildTarget target)
+    {
+        List<AssetBundleBuild> assetBundleBuilds = new List<AssetBundleBuild>();
+
+        string[] files = Directory.GetFiles(PathUtil.BuildResourcesPath, "*", SearchOption.AllDirectories);
+
+        for(int i = 0; i < files.Length; ++i)
+        {
+            if (files[i].EndsWith(".meta"))
+                continue;
+            string fileName = PathUtil.GetStandardPath(files[i]);
+            Debug.Log("files:" + fileName);
+            string assetName = PathUtil.GetUnityPath(fileName);
+            
+            AssetBundleBuild assetBundle = new AssetBundleBuild();
+            assetBundle.assetNames = new string[] { assetName };
+            string bundleName = fileName.Replace(PathUtil.BuildResourcesPath, "").ToLower();
+            assetBundle.assetBundleName = bundleName + ".ab";
+            assetBundleBuilds.Add(assetBundle);
+        }
+        if (Directory.Exists(PathUtil.BundleOutPath))
+        {
+            Directory.Delete(PathUtil.BundleOutPath, true);
+        }
+        Directory.CreateDirectory(PathUtil.BundleOutPath);
+        BuildPipeline.BuildAssetBundles(PathUtil.BundleOutPath, assetBundleBuilds.ToArray(), BuildAssetBundleOptions.None, target);
+    }
+}
---
+++ Assets/Scripts/Editor/PathUtil.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathUtil
+{
+    // RootDirectory
+    public static readonly string AssetsPath = Application.dataPath;
+
+    // The bundle directory
+    public static readonly string BuildResourcesPath = AssetsPath + "/BuildResources/"; //take care of the last '/'
+
+    // The output directory
+    public static readonly string BundleOutPath = Application.streamingAssetsPath;
+
+    public static string GetUnityPath(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+        {
+            return string.Empty;
+        }
+        return path.Substring(path.IndexOf("Assets"));
+    }
+
+    /// <summary>
+    /// Replace the "\\" of the path to "/"
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static string GetStandardPath(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+        {
+            return string.Empty;
+        }
+        return path.Trim().Replace("\\", "/");
+    }
+}
