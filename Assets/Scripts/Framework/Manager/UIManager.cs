e550f98b0bc915adc4fad8d996e24595c27f3570
Summer <790390404@qq.com>
2021-01-20 17:45:33 +0800

    设置UI分层，在UI实例化时设置其位置。

--- Assets/Scripts/Framework/Manager/UIManager.cs
+++ Assets/Scripts/Framework/Manager/UIManager.cs
@@ -6,10 +6,37 @@ public class UIManager : MonoBehaviour
 {
     // 保存UI
     Dictionary<string, GameObject> m_UI = new Dictionary<string, GameObject>();
-    GameObject Canvas;
-    public void OpenUI(string uiName,string luaName)
+    // UI分组
+    Dictionary<string, Transform> m_UIGroups = new Dictionary<string, Transform>();
+
+    private Transform m_UIParent;
+
+    private void Awake()
     {
-        Canvas = GameObject.Find("Canvas");
+        m_UIParent = this.transform.parent.Find("UI");
+    }
+
+    public void SetUIGroup(List<string> group)
+    {
+        for(int i = 0; i < group.Count; ++i)
+        {
+            GameObject go = new GameObject("Group-" + group[i]);
+            go.transform.SetParent(m_UIParent, false);
+            m_UIGroups.Add(group[i], go.transform);
+        }
+    }
+
+    Transform GetUIGroup(string groupName)
+    {
+        if (!m_UIGroups.ContainsKey(groupName))
+        {
+            Debug.LogError("Group \"" + groupName + "\" is not exisit");
+        }
+        return m_UIGroups[groupName].transform;
+    }
+
+    public void OpenUI(string uiName,string groupName,string luaName)
+    {
         // Awake的模拟
         // 要打开一个UI时先判断UI池中是否存在这个UI，如果有直接打开，如果没有先创建
         GameObject go = null;
@@ -24,8 +51,8 @@ public class UIManager : MonoBehaviour
              go = Instantiate(obj) as GameObject;
              m_UI.Add(uiName,go);
              UILogic uiLogic = go.AddComponent<UILogic>();
-             go.transform.SetParent(Canvas.transform);
-             go.transform.localPosition = Vector3.zero;
+             Transform parent = GetUIGroup(groupName);
+             go.transform.SetParent(parent,false);
              uiLogic.Init(luaName);     
              uiLogic.OnOpen();          // Start
          });
